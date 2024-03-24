3dec30d6e851c0a1cd9cc48e2c18309f460bd892
Matt Corthorne <72915519+MattC1313@users.noreply.github.com>
2024-03-24 15:59:36 +0000

    Delete Custom types practice.hs

--- Custom types practice.hs
+++
@@ -1,7 +0,0 @@
-data IntList = Empty | Cons Int IntList deriving(Show)
-
-data Tree = Leaf | Branch Tree Tree deriving (Show)
-
-data DTree a = DLeaf a
-             | DBranch a (DTree a) (DTree a)
-                                deriving (Show)
